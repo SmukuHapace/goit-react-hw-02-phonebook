4a962b38515e5cec6981d130cbecb9f91dbca699
Mateusz Kubiak <smuku.mk@gmail.com>
2023-06-30 19:24:49 +0200

    steps_1-4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import './App.css';
+
+import { ContactForm } from './ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: nanoid(), name: 'Rosie Simpson' },
+      { id: nanoid(), name: 'Hermione Kline' },
+      { id: nanoid(), name: 'Eden Clements' },
+      { id: nanoid(), name: 'Annie Copeland' },
+    ],
+  };
+
+  addContact = ({ name, number }) => {
+    const newContact = { id: nanoid(), name: name, number: number };
+    this.setState(e => ({
+      contacts: [...e.contacts, newContact],
+    }));
+  };
+
+  render() {
+    return (
+      <section>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <ul className="contacts-content">
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+}
